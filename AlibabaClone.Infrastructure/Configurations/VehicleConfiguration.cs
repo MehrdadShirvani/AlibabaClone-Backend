91ae963b1a90d0b9aa806e9294be44d8d6fb242b
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 13:53:00 +0330

    feat: add configuration files for vehicles

---
+++ AlibabaClone.Infrastructure/Configurations/VehicleConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using AlibabaClone.Domain.Aggregates.VehicleAggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations
+{
+    public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.HasKey(v => v.Id);
+            builder.Property(v => v.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(v => v.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(v => v.VehicleTypeId)
+                .IsRequired();
+
+            builder.Property(v => v.Capacity)
+                .IsRequired();
+
+            builder.Property(v => v.PlateNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.HasIndex(v => v.PlateNumber).IsUnique();
+
+
+            //Relationships
+            builder.HasOne(v => v.VehicleType)
+                .WithMany(vt => vt.Vehicles)
+                .HasForeignKey(v => v.VehicleTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ AlibabaClone.Infrastructure/Configurations/VehicleTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Aggregates.CompanyAggregates;
+using AlibabaClone.Domain.Aggregates.LocationAggregates;
+using AlibabaClone.Domain.Aggregates.VehicleAggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations
+{
+    public class VehicleTypeConfiguration : IEntityTypeConfiguration<VehicleType>
+    {
+        public void Configure(EntityTypeBuilder<VehicleType> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasIndex(c => c.Title)
+                .IsUnique();
+        }
+    }
+}
