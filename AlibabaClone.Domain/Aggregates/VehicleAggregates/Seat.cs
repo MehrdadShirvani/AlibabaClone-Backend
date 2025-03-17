cab1758b94f04e260b12e3fab80bc09f82d0c005
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 10:08:29 +0330

    feat: add Seat, Vehicle, and VehicleType entities

---
+++ AlibabaClone.Domain/Aggregates/VehicleAggregates/Seat.cs
@@ -0,0 +1,23 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AlibabaClone.Domain.Framework.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.VehicleAggregates
+{
+    public class Seat : Entity<long>
+    {
+        public int VehicleId { get; set; }
+        public int Row {  get; set; }   
+        public int Column { get; set; }
+        public bool IsVIP { get; set; }
+        public bool IsAvailable { get; set; }
+        public string? Description { get; set; }
+
+        public virtual Vehicle Vehicle { get; set; }
+        public virtual ICollection<Ticket> Tickets { get; set; }    
+    }
+}
