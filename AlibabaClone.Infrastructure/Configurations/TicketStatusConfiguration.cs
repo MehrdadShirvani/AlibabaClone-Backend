e5beb9579125adc30525a1579f14ca82df8e5283
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 14:26:30 +0330

    feat: add configuration files for transportation related entities

---
+++ AlibabaClone.Infrastructure/Configurations/TicketStatusConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations
+{
+    public class TicketStatusConfiguration : IEntityTypeConfiguration<TicketStatus>
+    {
+        public void Configure(EntityTypeBuilder<TicketStatus> builder)
+        {
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(r => r.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasIndex(r => r.Title)
+                .IsUnique();
+        }
+    }
+}
