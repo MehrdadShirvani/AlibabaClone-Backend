2a573232b2b3a10fecc37ba9728d1967ac1adcd9
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 15:40:55 +0330

    refactor(PersonConfiguration): specyfying column type of strings

--- AlibabaClone.Infrastructure/Configurations/PersonConfiguration.cs
+++ AlibabaClone.Infrastructure/Configurations/PersonConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlibabaClone.Infrastructure.Configurations
 {
@@ -19,29 +14,30 @@ namespace AlibabaClone.Infrastructure.Configurations
 
             builder.Property(p => p.FirstName)
                 .IsRequired()
-                .HasColumnType("nvarchar(50)");
+                .HasMaxLength(50);
 
             builder.Property(p => p.LastName)
                 .IsRequired()
-                .HasColumnType("nvarchar(50)");
+                .HasMaxLength(50);
+
 
             builder.Property(p => p.IdNumber)
                 .IsRequired()
                 .HasMaxLength(20)
-                .HasColumnType("varchar(20)");
+                .IsUnicode(false);
             builder.HasIndex(p => p.IdNumber).IsUnique();
 
             builder.Property(p => p.PassportNumber)
                 .HasMaxLength(20)
-                .HasColumnType("varchar(20)");
+                .IsUnicode(false);
 
             builder.Property(p => p.EnglishFirstName)
                 .HasMaxLength(50)
-                .HasColumnType("varchar(50)");
+                .IsUnicode(false);
 
             builder.Property(p => p.EnglishLastName)
                 .HasMaxLength(50)
-                .HasColumnType("varchar(50)");
+                .IsUnicode(false);
 
             builder.Property(p => p.Birthdate)
                 .HasColumnType("date");
@@ -50,8 +46,8 @@ namespace AlibabaClone.Infrastructure.Configurations
             builder.HasOne(p => p.Gender)
                 .WithMany()
                 .HasForeignKey(p => p.GenderId)
-                .OnDelete(DeleteBehavior.Restrict); 
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
-    }
+}
 }
