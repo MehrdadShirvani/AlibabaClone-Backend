b1fcf076001d377e28c0767473bad7142d894e05
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 13:17:35 +0330

    feat: add configuration files for company related entities

---
+++ AlibabaClone.Infrastructure/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Aggregates.CompanyAggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasIndex(c => c.Title)
+                .IsUnique();
+        }
+    }
+}
