5ed2ef89e0866121c3a561d637ad6b494bc31588
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-03-17 10:06:31 +0330

    feat: add Company entity

---
+++ AlibabaClone.Domain/Aggregates/CompanyAggregates/Company.cs
@@ -0,0 +1,18 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AlibabaClone.Domain.Framework.Base;
+using AlibabaClone.Domain.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Domain.Aggregates.CompanyAggregates
+{
+    public class Company : Entity<int>
+    {
+        public required string Title { get; set; }
+
+        public virtual ICollection<Transportation> Transportations { get; set; }
+    }
+}
