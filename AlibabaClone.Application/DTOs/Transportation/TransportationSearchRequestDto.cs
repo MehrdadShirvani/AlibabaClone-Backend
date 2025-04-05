ce1da8482a046c03b5043ced735b7c5fd69343ed
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-05 23:56:11 +0330

    feat: add transportation search dtos

---
+++ AlibabaClone.Application/DTOs/Transportation/TransportationSearchRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AlibabaClone.Application.DTOs.Transportation
+{
+    public class TransportationSearchRequestDto
+    {
+        public int? FromCityId { get; init; }
+        public int? ToCityId { get; init; }
+        public DateTime? StartDate { get; init; }
+        public DateTime? EndDate { get; init; }
+    }
+}
---
+++ AlibabaClone.Application/DTOs/Transportation/TransportationSearchResultDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AlibabaClone.Application.DTOs.Transportation
+{
+    public class TransportationSearchResultDto
+    {
+        public int Id { get; init; }
+        public string CompanyTitle { get; init; }   
+        public string FromLocationTitle { get; init; }
+        public string ToLocationTitle { get; init; }
+        public string FromCityTitle { get; init; }
+        public string ToCityTitle { get; init; }
+        public DateTime StartDateTime { get; init; }
+        public DateTime EndDateTime{ get; init; }
+        public decimal Price { get; init; } 
+    }
+}
