835f988a1002bbcb291ea7a81ad14a7d1a6aa9dc
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-05 23:35:00 +0330

    feat(ITranportationRepository, TransportationRepository): add SearchTransportationsAsync function

--- AlibabaClone.Infrastructure/Services/TransportationAggregates/TransportationRepository.cs
+++ AlibabaClone.Infrastructure/Services/TransportationAggregates/TransportationRepository.cs
@@ -1,6 +1,8 @@
 ﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
 using AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories;
 using AlibabaClone.Infrastructure.Framework.Base;
+using Castle.Components.DictionaryAdapter.Xml;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlibabaClone.Infrastructure.Services.TransportationAggregates
 {
@@ -11,6 +13,19 @@ namespace AlibabaClone.Infrastructure.Services.TransportationAggregates
         public TransportationRepository(ApplicationDBContext dbContext) : base(dbContext)
         {
 
+        }
+
+        public async Task<List<Transportation>> SearchTransportationsAsync(int? fromCityId, int? toCityId, DateTime? startDate, DateTime? endDate)
+        {
+            var query = DbContext.Transportations.Include(x => x.FromLocation).Include(x => x.ToLocation).AsQueryable();
+            
+            query = query.Where(x => fromCityId == null || x.FromLocation.CityId == fromCityId.Value);
+            query = query.Where(x => toCityId == null || x.ToLocation.CityId == toCityId.Value);
+            query = query.Where(x => startDate == null || x.StartDateTime.Date == startDate.Value.Date);
+            query = query.Where(x => endDate == null || x.EndDateTime.Date == endDate.Value.Date);
+            query = query.Include(x => x.FromLocation).ThenInclude(x => x.City);
+            query = query.Include(x => x.ToLocation).ThenInclude(x => x.City);
+            return await query.Include(x => x.Company).ToListAsync();
         }
     }
 }
