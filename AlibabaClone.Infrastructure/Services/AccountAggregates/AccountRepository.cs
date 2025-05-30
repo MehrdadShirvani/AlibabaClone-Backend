b079d2c96276ee603877b7480c527558f22ea10e
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:20:08 +0330

    feat: add method to get user by phone number

--- AlibabaClone.Infrastructure/Services/AccountAggregates/AccountRepository.cs
+++ AlibabaClone.Infrastructure/Services/AccountAggregates/AccountRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
 using AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories;
 using AlibabaClone.Infrastructure.Framework.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlibabaClone.Infrastructure.Services.AccountAggregates
 {
@@ -11,6 +12,12 @@ namespace AlibabaClone.Infrastructure.Services.AccountAggregates
         public AccountRepository(ApplicationDBContext dbContext) : base(dbContext)
         {
 
+        }
+
+        public async Task<Account> GetByPhoneNumberAsync(string phoneNumber)
+        {
+            var user = await DbContext.Accounts.Include(x => x.AccountRoles).ThenInclude(x => x.Role).FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber);
+            return user;
         }
     }
 }
