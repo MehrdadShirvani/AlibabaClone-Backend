261aac8bd4a7369b33249b1e4fd03ca787256b74
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:16:08 +0330

    feat: add JWT Genrator and JWTSettings

---
+++ AlibabaClone.WebAPI/Authentication/JwtGenerator.cs
@@ -0,0 +1,45 @@
+﻿using AlibabaClone.Application.DTOs.Account;
+using AlibabaClone.Application.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AlibabaClone.WebAPI.Authentication
+{
+    public class JwtGenerator : IJwtGenerator
+    {
+        private readonly JwtSettings _jwtSettings;
+        public JwtGenerator(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+        public string GenerateToken(AccountDTO accountDto)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, accountDto.Id.ToString()),
+                new Claim("phone", accountDto.PhoneNumber)
+            };
+
+            foreach (var role in accountDto.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
