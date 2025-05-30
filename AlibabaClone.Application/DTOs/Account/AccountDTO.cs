e04e0bc5146d500b7f9342a5424b8a9c1f369955
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 18:47:18 +0330

    fix: change the name of AccountDTO to AccountDto

--- AlibabaClone.Application/DTOs/Account/AccountDTO.cs
+++ AlibabaClone.Application/DTOs/Account/AccountDTO.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlibabaClone.Application.DTOs.Account
+﻿namespace AlibabaClone.Application.DTOs.Account
 {
-    public class AccountDTO
+    public class AccountDto
     {
         public long Id { get; set; }
         public required string PhoneNumber { get; set; }
