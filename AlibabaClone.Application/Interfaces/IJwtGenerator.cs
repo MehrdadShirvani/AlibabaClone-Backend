4d74ac7bd32522af755de753f8591a4c594e0a61
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:16:24 +0330

    feat: add IJwtGenerator

---
+++ AlibabaClone.Application/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,9 @@
+﻿using AlibabaClone.Application.DTOs.Account;
+
+namespace AlibabaClone.Application.Interfaces
+{
+    public interface IJwtGenerator
+    {
+        string GenerateToken(AccountDTO accountDTO);
+    }
+}
