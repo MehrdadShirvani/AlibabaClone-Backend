ee669dd571d33ecfb79141e4651b045921bce7d9
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:14:44 +0330

    feat: add AccountDto

---
+++ AlibabaClone.Application/DTOs/Account/AccountDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.DTOs.Account
+{
+    public class AccountDTO
+    {
+        public long Id { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { set; get; }
+        public string? Email { get; set; }
+        public long? PersonId { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
