39d3b0059abe5ff6a21a86f9617c739126bcf8e9
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:15:07 +0330

    feat: add authentication-related DTOs

---
+++ AlibabaClone.Application/DTOs/Authentication/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AlibabaClone.Application.DTOs.Authentication
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public List<string> Roles { get; set; }
+    }
+}
