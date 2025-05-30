810e0ca159020d8f0ce391c82446a296d2acb504
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:54:42 +0330

    fix: add id to AuthResponseDto

--- AlibabaClone.Application/DTOs/Authentication/AuthResponseDto.cs
+++ AlibabaClone.Application/DTOs/Authentication/AuthResponseDto.cs
@@ -2,6 +2,7 @@
 {
     public class AuthResponseDto
     {
+        public long Id { get; set; }
         public string Token { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
         public List<string> Roles { get; set; }
