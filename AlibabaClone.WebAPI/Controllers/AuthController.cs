5ce72c6aa1fe850f631ee782b1bdd6de11fbabf4
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 17:21:23 +0330

    feat: add AuthController

---
+++ AlibabaClone.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using AlibabaClone.Application.DTOs.Authentication;
+using AlibabaClone.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            var result = await _authService.LoginAsync(request);
+            if (!result.IsSuccess)
+                return BadRequest(result.ErrorMessage);
+
+            return Ok(result.Data);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            var result = await _authService.RegisterAsync(request);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.ErrorMessage);
+
+            return Ok(result.Data);
+        }
+    }
+}
