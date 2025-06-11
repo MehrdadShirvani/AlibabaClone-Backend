c8744583bce11e30f3c7bd7bd0d1e9b93bcbddcf
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 16:46:32 +0330

    fix: adjust Ticket considering TicketOrder exists

--- AlibabaClone.Domain/Aggregates/TransportationAggregates/Ticket.cs
+++ AlibabaClone.Domain/Aggregates/TransportationAggregates/Ticket.cs
@@ -11,20 +11,20 @@ namespace AlibabaClone.Domain.Aggregates.TransportationAggregates
 {
     public class Ticket : Entity<long>
     {
-        public long TransportationId { get; set; }
+            
+        public long TicketOrderId { get; set; }
         public long SeatId { get; set; }
-        public long BuyerId { get; set; }
         public long TravelerId { get; set; }
         public DateTime CreatedAt { get; set; }
+        public DateTime? CanceledAt { get; set; }
         public long? CompanionId { get; set; }   
         public short TicketStatusId { get; set; }
         public string SerialNumber { get; protected set; }
         public string? Description { get; set; }
 
 
-        public virtual Transportation Transportation { get; set; }  
-        public virtual Seat Seat { get; set; }  
-        public virtual Account Buyer { get; set; }    
+        public virtual TicketOrder TicketOrder { get; set; }  
+        public virtual Seat Seat { get; set; }
         public virtual Person Traveler { get; set; }
         public virtual Person? Companion { get; set; }
         public virtual TicketStatus TicketStatus { get; set; }
