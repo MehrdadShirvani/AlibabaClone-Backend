92e481640d4084a4653bd62d7ad64cb743c7d177
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 14:20:32 +0330

    feat: add migrations

---
+++ AlibabaClone.Infrastructure/Migrations/20250611104954_Adjust Database for Profile Page.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlibabaClone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustDatabaseforProfilePage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TicketId",
+                table: "Transactions",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "TransactionTypeId",
+                table: "Transactions",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "People",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentBalance",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "BankAccountDetail",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<long>(type: "bigint", nullable: false),
+                    BankName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    CardNumber = table.Column<string>(type: "varchar(16)", unicode: false, maxLength: 16, nullable: true),
+                    IBAN = table.Column<string>(type: "varchar(24)", unicode: false, maxLength: 24, nullable: true),
+                    BankAccountNumber = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccountDetail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccountDetail_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionType",
+                columns: table => new
+                {
+                    Id = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionType", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccountDetail_AccountId",
+                table: "BankAccountDetail",
+                column: "AccountId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionType_Title",
+                table: "TransactionType",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_TransactionType_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId",
+                principalTable: "TransactionType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_TransactionType_TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccountDetail");
+
+            migrationBuilder.DropTable(
+                name: "TransactionType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionTypeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentBalance",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TicketId",
+                table: "Transactions",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
