a424a58949e27f765de6a8f2292b1f9ea65048e9
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 18:14:21 +0330

    feat: complete profile method in AccountController

--- AlibabaClone.WebAPI/Controllers/AccountController.cs
+++ AlibabaClone.WebAPI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Application.Result;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlibabaClone.WebAPI.Controllers
@@ -8,11 +9,32 @@ namespace AlibabaClone.WebAPI.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IUserContext _userContext;
+        private readonly IAccountService _accountService;
+
+        public AccountController(IUserContext userContext, IAccountService accountService)
+        {
+            _userContext = userContext;
+            this._accountService = accountService;
+        }
+
         [Authorize(Roles = "User")]
         [HttpGet("profile")]
-        public IActionResult GetProfile()
+        public async Task<IActionResult> GetProfile()
         {
-            return Ok("Hi there, hello");
+            long userId = _userContext.GetUserId();
+            var result = await _accountService.GetProfileAsync(userId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+
+            return result.Status switch
+            {
+                ResultStatus.NotFound => NotFound(result.ErrorMessage),
+                ResultStatus.ValidationError => BadRequest(result.ErrorMessage),
+                _ => StatusCode(500, result.ErrorMessage)
+            };
         }
     }
 }
