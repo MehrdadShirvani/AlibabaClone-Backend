2c1c3d92927a41a3d1d8962aef62796fbfc4a27d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 19:47:36 +0330

    feat: add TravelerTicketDto

---
+++ AlibabaClone.Application/DTOs/Transportation/TravelerTicketDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.DTOs.Transportation
+{
+    public class TravelerTicketDto
+    {
+        public long Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string TravelerName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string SeatNumber { get; set; }
+        public string TicketStatus { get; set; }
+        public string CompanionName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
