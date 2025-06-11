358b6dcbfa1427fdc5a6d4df5eaaa754cadbd8dc
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 18:52:35 +0330

    feat: add GetTravels to IAccountService and implement it in AccountService

--- AlibabaClone.Application/Interfaces/IAccountService.cs
+++ AlibabaClone.Application/Interfaces/IAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using AlibabaClone.Application.DTOs.Account;
+using AlibabaClone.Application.DTOs.Transportation;
 using AlibabaClone.Application.Result;
 
 namespace AlibabaClone.Application.Interfaces
@@ -6,5 +7,6 @@ namespace AlibabaClone.Application.Interfaces
     public interface IAccountService
     {
         Task<Result<ProfileDto>> GetProfileAsync(long accountId);
+        Task<Result<List<TicketOrderSummaryDto>>> GetTravels(long accountId);
     }
 }
--- AlibabaClone.Application/Services/AccountService.cs
+++ AlibabaClone.Application/Services/AccountService.cs
@@ -2,23 +2,28 @@
 using AlibabaClone.Application.Interfaces;
 using AlibabaClone.Application.Result;
 using AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories;
 using AlibabaClone.Domain.Framework.Interfaces;
 using AutoMapper;
 using System.Runtime.CompilerServices;
+using AlibabaClone.Application.DTOs.Transportation;
+using System.Collections.Generic;
 
 namespace AlibabaClone.Application.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly ITicketOrderRepository _ticketOrderRepository;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
 
-        public AccountService(IAccountRepository accountRepository, IMapper mapper, IUnitOfWork unitOfWork) 
+        public AccountService(IAccountRepository accountRepository, IMapper mapper, IUnitOfWork unitOfWork, ITicketOrderRepository ticketOrderRepository)
         {
             _accountRepository = accountRepository;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+            _ticketOrderRepository = ticketOrderRepository;
         }
 
         public async Task<Result<ProfileDto>> GetProfileAsync(long accountId)
@@ -32,5 +37,15 @@ namespace AlibabaClone.Application.Services
             return Result<ProfileDto>.NotFound(null);
         }
 
+        public async Task<Result<List<TicketOrderSummaryDto>>> GetTravels(long accountId)
+        {
+            var result = await _ticketOrderRepository.GetAllByBuyerId(accountId);
+            if (result != null)
+            {
+                return Result<List<TicketOrderSummaryDto>>.Success(_mapper.Map<List<TicketOrderSummaryDto>>(result));
+            }
+
+            return Result<List<TicketOrderSummaryDto>>.NotFound(null);
+        }
     }
 }
