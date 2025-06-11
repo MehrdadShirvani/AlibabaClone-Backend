8abd78ae58608e20c239c35b3d919f83137340f8
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 20:41:35 +0330

    feat: add EditPasswordDtp

---
+++ AlibabaClone.Application/DTOs/Account/EditPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlibabaClone.Application.DTOs.Account
+{
+    public class EditPasswordDto
+    {
+        [Required(ErrorMessage = "Old Password is required.")]
+        public string OldPassword {  get; set; }
+
+        [Required(ErrorMessage = "New Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string NewPassword { get; set; }
+        [Compare(nameof(NewPassword), ErrorMessage = "New Passwords do not match.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
