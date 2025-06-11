eb29ecbbb847c120a7c54682ee0a466b81080d45
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 20:29:04 +0330

    feat: add EditEmailDto

---
+++ AlibabaClone.Application/DTOs/Account/EditEmailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.DTOs.Account
+{
+    public class EditEmailDto
+    {
+        [EmailAddress(ErrorMessage = "Email is not valid")]
+        public string NewEmail { get; set; }
+    }
+}
