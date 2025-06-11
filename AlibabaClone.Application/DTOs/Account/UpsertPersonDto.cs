ba5d33436703de90c3cea6c2309480602d824e71
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 21:01:19 +0330

    feat: add UpsertPersonDto

---
+++ AlibabaClone.Application/DTOs/Account/UpsertPersonDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.DTOs.Account
+{
+    public class UpsertPersonDto
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "National ID number is required.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "National ID number must be exactly 10 digits.")]
+        public string IdNumber { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+        public short GenderId{ get; set; }
+        //[Required(ErrorMessage = "Phone number is required.")]
+        //[Phone(ErrorMessage = "Phone number format is not valid.")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Birth date is required.")]
+        public DateTime BirthDate { get; set; }
+        public string EnglishFirstName { get; set; }
+        public string EnglishLastName { get; set; }
+        
+    }
+}
