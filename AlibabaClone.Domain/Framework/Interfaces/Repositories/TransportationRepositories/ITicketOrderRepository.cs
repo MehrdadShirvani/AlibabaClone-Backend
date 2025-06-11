b513807bf19fdfe6a11ba29d0c326f8737ae4ef7
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 18:49:53 +0330

    feat: add ITicketOrderRepository and TicketOrderRepository and add GetAllBuBuyerId method

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransportationRepositories/ITicketOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories
+{
+    public interface ITicketOrderRepository : IRepository<TicketOrder, long>
+    {
+        public Task<List<TicketOrder>> GetAllByBuyerId(long buyerId);
+    }
+}
