c2148c61db8f7a1d52380df25c288f08cbb814fe
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 14:14:37 +0330

    feat: add BankAccountDetail in Account and add current balance

--- AlibabaClone.Domain/Aggregates/AccountAggregates/Account.cs
+++ AlibabaClone.Domain/Aggregates/AccountAggregates/Account.cs
@@ -9,9 +9,30 @@ namespace AlibabaClone.Domain.Aggregates.AccountAggregates
         public required string Password { set; get; }
         public string? Email { get; set; }
         public long? PersonId { get; set; }
+        public decimal CurrentBalance { get; private set; }
+
 
         public virtual Person? Person { get; set; }
+        public virtual BankAccountDetail? BankAccountDetail { get; set; }
         public virtual ICollection<Ticket> BoughtTickets { get; set; }
         public virtual ICollection<AccountRole> AccountRoles { get; set; }
+
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new InvalidOperationException("Amount must be positive.");
+
+            CurrentBalance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0 || amount > CurrentBalance)
+                throw new InvalidOperationException("Invalid withdrawal amount.");
+
+            CurrentBalance -= amount;
+        }
+
     }
 }
