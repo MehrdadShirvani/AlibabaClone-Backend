1e89a893857309b45396d83705c181af61270b73
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 16:54:53 +0330

    chore: add migrations

--- AlibabaClone.Infrastructure/Migrations/20250611132032_Add TicketOrder And Coupon.Designer.cs
+++ AlibabaClone.Infrastructure/Migrations/20250611132032_Add TicketOrder And Coupon.Designer.cs
@@ -4,6 +4,7 @@ using AlibabaClone.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlibabaClone.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250611132032_Add TicketOrder And Coupon")]
+    partial class AddTicketOrderAndCoupon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
