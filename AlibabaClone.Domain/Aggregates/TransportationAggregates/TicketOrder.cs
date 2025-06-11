3c5cccedd20bf3bcf447906fd4ef5e1fafd1eb53
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 16:46:09 +0330

    feat: add TicketOrder

---
+++ AlibabaClone.Domain/Aggregates/TransportationAggregates/TicketOrder.cs
@@ -0,0 +1,18 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Framework.Base;
+
+namespace AlibabaClone.Domain.Aggregates.TransportationAggregates
+{
+    public class TicketOrder : Entity<long>
+    {
+        public long TransportationId { get; set; }
+        public long BuyerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string SerialNumber { get; protected set; }
+        public string? Description { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+        public virtual Transportation Transportation { get; set; }
+        public virtual Account Buyer { get; set; }
+    }
+}
