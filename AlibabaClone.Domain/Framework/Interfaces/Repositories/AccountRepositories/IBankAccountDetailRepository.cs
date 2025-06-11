339f8bd25c617cfc2a1be64ae783fdb7fe36db95
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 21:17:13 +0330

    feat: add IBankAccountDetailRepository and BankAccountDetailRepository add GetByAccountIdAsync method in IBankAccountDetailRepository  and implement it

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/AccountRepositories/IBankAccountDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories
+{
+    public interface IBankAccountDetailRepository : IRepository<BankAccountDetail, long>
+    {
+        Task<BankAccountDetail> GetByAccountIdAsync(long accountId);
+    }
+}
---
+++ AlibabaClone.Infrastructure/Services/AccountAggregates/BankAccountDetailRepository.cs
@@ -0,0 +1,22 @@
+﻿using AlibabaClone.Domain.Aggregates.AccountAggregates;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories;
+using AlibabaClone.Infrastructure.Framework.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlibabaClone.Infrastructure.Services.AccountAggregates
+{
+    public class BankAccountDetailRepository :
+        BaseRepository<ApplicationDBContext, BankAccountDetail, long>,
+        IBankAccountDetailRepository
+    {
+        public BankAccountDetailRepository(ApplicationDBContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<BankAccountDetail> GetByAccountIdAsync(long accountId)
+        {
+            return await DBSet.FirstOrDefaultAsync(x => x.AccountId == accountId);
+        }
+    }
+}
