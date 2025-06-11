e40ee729650bcb67dd34aa1b96dc2f4dff4b1328
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-11 19:49:19 +0330

    feat: add TransactionDto

---
+++ AlibabaClone.Application/DTOs/Transaction/TransactionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AlibabaClone.Application.DTOs.Transaction
+{
+    public class TransactionDto
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public short TransactionTypeId { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+        public decimal FinalAmount { get; set; }
+    }
+}
