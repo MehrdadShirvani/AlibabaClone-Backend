61ec19e56c674146494a5ca17f50c15712f3f7c0
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-17 11:36:23 +0330

    feat: add GetSeatsByVehicleId to ISeatRepostory and implement it

--- AlibabaClone.Infrastructure/Services/VehicleAggregates/SeatRepository.cs
+++ AlibabaClone.Infrastructure/Services/VehicleAggregates/SeatRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AlibabaClone.Domain.Aggregates.VehicleAggregates;
 using AlibabaClone.Domain.Framework.Interfaces.Repositories.VehicleRepositories;
 using AlibabaClone.Infrastructure.Framework.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlibabaClone.Infrastructure.Services.VehicleAggregates
 {
@@ -11,6 +12,13 @@ namespace AlibabaClone.Infrastructure.Services.VehicleAggregates
         public SeatRepository(ApplicationDBContext dbContext) : base(dbContext)
         {
 
+        }
+
+        public async Task<List<Seat>> GetSeatsByVehicleId(long vehicleId)
+        {
+            return await DBSet.Include(x => x.Vehicle)
+                              .Include(x => x.Tickets).ThenInclude(x => x.Traveler)
+                              .Where(x => x.VehicleId == vehicleId).ToListAsync();
         }
     }
 }
