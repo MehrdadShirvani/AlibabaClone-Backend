a70fc8306468b67aec485da7a4688a01f3915c2a
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-18 20:11:09 +0330

    feat: add IPersonService and implement it

---
+++ AlibabaClone.Application/Interfaces/IPersonService.cs
@@ -0,0 +1,11 @@
+﻿using AlibabaClone.Application.DTOs.Account;
+using AlibabaClone.Application.Result;
+
+namespace AlibabaClone.Application.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<Result<long>> UpsertAccountPersonAsync(long accountId, PersonDto dto); // Returns PersonId
+        Task<Result<long>> UpsertPersonAsync(long accountId, PersonDto dto); // Returns PersonId
+    }
+}
