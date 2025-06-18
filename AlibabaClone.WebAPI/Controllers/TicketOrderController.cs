4fa0056c46b3fb811e189cf381d217a6852e837a
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-18 20:27:05 +0330

    feat: add TicketOrderController

---
+++ AlibabaClone.WebAPI/Controllers/TicketOrderController.cs
@@ -0,0 +1,15 @@
+﻿using AlibabaClone.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketOrderController : ControllerBase
+    {
+        private readonly IUserContext _userContext;
+        private readonly ITransportationService _transportationService;
+
+    }
+}
