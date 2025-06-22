015c1c4cc1a1f8cf5c5f8d1ba5be27fdbfce0cf1
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-22 13:59:30 +0330

    feat: install QuestPDF in Infrastructure and add IPdfGenerator and implement it

---
+++ AlibabaClone.Application/Interfaces/IPdfGenerator.cs
@@ -0,0 +1,9 @@
+﻿using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+
+namespace AlibabaClone.Application.Interfaces
+{
+    public interface IPdfGenerator
+    {
+        byte[] GenerateTicketsPdf(TicketOrder ticketOrder);
+    }
+}
---
+++ AlibabaClone.Infrastructure/Services/PdfGenerator.cs
@@ -0,0 +1,45 @@
+﻿using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace AlibabaClone.Infrastructure.Services
+{
+    public class PdfGenerator : IPdfGenerator
+    {
+        public byte[] GenerateTicketsPdf(TicketOrder ticketOrder)
+        {
+            var document = Document.Create(container =>
+        {
+            foreach (var ticket in ticketOrder.Tickets)
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.Content().Column(col =>
+                    {
+                        col.Item().Text($"Ticket: {ticket.SerialNumber}").FontSize(20);
+                        col.Item().Text($"Passenger: {ticket.Traveler.FirstName} {ticket.Traveler.LastName}");
+                        col.Item().Text($"Seat: {ticket.Seat.Row} - {ticket.Seat.Column}");
+                        col.Item().Text($"Trip: {ticketOrder.Transportation.FromLocation.City.Title} {ticketOrder.Transportation.ToLocation.City.Title}");
+                    });
+                });
+            }
+        });
+
+            using var stream = new MemoryStream();
+            try
+            {
+
+            document.GeneratePdf(stream);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return stream.ToArray();
+        }
+    }
+}
