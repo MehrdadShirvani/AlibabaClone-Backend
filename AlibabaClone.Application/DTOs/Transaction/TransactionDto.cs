e13f2f8dfed269463d5c5c22af336f016c4d0f1f
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-27 12:12:01 +0330

    feat: add CouponId to TransactionDto

--- AlibabaClone.Application/DTOs/Transaction/TransactionDto.cs
+++ AlibabaClone.Application/DTOs/Transaction/TransactionDto.cs
@@ -4,6 +4,7 @@
         {
             public long Id { get; set; }
             public long? TicketOrderId { get; set; }
+            public long? CouponId { get; set; } 
             public DateTime CreatedAt { get; set; }
             public short TransactionTypeId { get; set; }
             public string TransactionType { get; set; }
