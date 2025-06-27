999f6bc9a76309d28215822284642450a085fca7
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-27 12:19:33 +0330

    feat: add ValidateCoupon tp CouponController

---
+++ AlibabaClone.WebAPI/Controllers/CouponController.cs
@@ -0,0 +1,40 @@
+﻿using AlibabaClone.Application.DTOs.Transaction;
+using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Application.Result;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "User")]
+    public class CouponController : ControllerBase
+    {
+        private readonly IUserContext _userContext;
+        private readonly ICouponService _couponService;
+        public CouponController(IUserContext userContext, ICouponService couponService)
+        {
+            _userContext = userContext;
+            _couponService = couponService;
+        }
+
+        [HttpPost("validate")]
+        public async Task<IActionResult> ValidateCoupon(CouponValidationRequestDto dto)
+        {
+            long accountId = _userContext.GetUserId();
+            if (accountId <= 0) return Unauthorized();
+            var result = await _couponService.ValidateCouponAsync(accountId, dto);
+            return result.Status switch
+            {
+                ResultStatus.Success => Ok(result.Data),
+                ResultStatus.Unauthorized => Unauthorized(result.ErrorMessage),
+                ResultStatus.NotFound => NotFound(result.ErrorMessage),
+                ResultStatus.ValidationError => BadRequest(result.ErrorMessage),
+                _ => StatusCode(500, result.ErrorMessage)
+            };
+        }
+    }
+}
