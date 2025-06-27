fc7e2bf917d8861ea85d778fe4f3ebde0595c54d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-27 12:16:10 +0330

    feat: add ICouponRepository and implement it

---
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/TransactionRepositories/ICouponRepository.cs
@@ -0,0 +1,11 @@
+﻿using AlibabaClone.Domain.Aggregates.TransactionAggregates;
+
+namespace AlibabaClone.Domain.Framework.Interfaces.Repositories.TransactionRepositories
+{
+    public interface ICouponRepository : IRepository<Coupon, long> 
+    {
+        Task<Coupon?> GetByCodeAsync(string code);
+        Task<int> GetTotalUsagesAsync(string code);
+        Task<int> GetUsagesByAccountAsync(string code, long accountId);
+    }
+}
